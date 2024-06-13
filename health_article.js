56d089636f8a04d5ed2e6a697975eea409c0d6dd
Engdawerk <116351980+Engdawerk@users.noreply.github.com>
2024-06-13 18:40:58 +0300

    health_article.js

--- health_article.js
+++ health_article.js
@@ -1,3 +1,8 @@
 var xhr = new XMLHttpRequest();
 var url = './health.json';
 xhr.open('GET', url, true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+  var articles = xhr.response.articles;
+var articlesDiv = document.getElementById('articles');
+}
